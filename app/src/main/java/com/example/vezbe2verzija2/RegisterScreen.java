c8a021b1c2b7985ecf0a25e43cfb707cb9e1c408
aleksavelickovic <velickovic.sr44.2023@uns.ac.rs>
2025-08-30 00:22:49 +0200

    Added and used new strings in strings.xml...
    Added register functionality to RegisterScreen.java
    
    Refactored Home and Login screen

--- app/src/main/java/com/example/vezbe2verzija2/RegisterScreen.java
+++ app/src/main/java/com/example/vezbe2verzija2/RegisterScreen.java
@@ -1,6 +1,10 @@
 package com.example.vezbe2verzija2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +19,22 @@ public class RegisterScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register_screen);
+
+        Button potvrdibtn = findViewById(R.id.potvrdibutton);
+        EditText emailtext = findViewById(R.id.editTextTextEmailAddress);
+        EditText pswtext = findViewById(R.id.editTextTextPassword);
+        potvrdibtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterScreen.this, HomeScreen.class);
+                intent.putExtra("email", emailtext.getText().toString());
+                intent.putExtra("password", pswtext.getText().toString());
+                startActivity(intent);
+            }
+        });
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
